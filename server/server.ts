680669dac0a387f8b908588287e95914f53b5baa
Laura <brejcakova.laura@gmail.com>
2024-09-19 23:12:26 +0200

    install payload

--- server/server.ts
+++ server/server.ts
@@ -2,9 +2,9 @@ import express from 'express';
 import cors from 'cors';  
 import todoRoutes from './routes/todoRoutes';
 import { connectDB } from './config/db';
-import dotenv from 'dotenv';
+import payload from 'payload';
 
-dotenv.config();
+require('dotenv').config()
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -18,6 +18,16 @@ connectDB();
 // Use routes
 app.use('/todos', todoRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const start = async () => {
+  await payload.init({
+    secret: process.env.PAYLOAD_SECRET,
+    express: app,
+    mongoURL: process.env.MONGO_URI, 
+  });
+
+  app.listen(PORT, async () => {
+    console.log(`Express is now listening for incoming connections on port ${PORT}`);
+  });
+};
+
+start();
