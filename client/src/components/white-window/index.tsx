7113176ba26412fba1a249cc1f5b164123eefc2a
laura <brejcakova.laura@gmail.com>
2025-02-10 22:32:52 +0100

    testDetail update

--- client/src/components/white-window/index.tsx
+++ client/src/components/white-window/index.tsx
@@ -1,20 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
-import {
-  IconButton,
-  Box,
-  Typography,
-  Link,
-  Button,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-  Card,
-} from '@mui/material'
+import { IconButton, Box, Typography, Link, Card } from '@mui/material'
 import * as XLSX from 'xlsx'
 import ParsedInbodyTest from './parsed-inbody/index'
 import { useNavigate } from 'react-router-dom'
@@ -22,6 +8,7 @@ import params from '../../data/inbody-params.json'
 
 const TestDetailWindow: React.FC<{ result: any }> = ({ result }) => {
   const [parsedData, setParsedData] = useState<any[]>([])
+  const [showDetails, setShowDetails] = useState(true)
   const navigate = useNavigate()
 
   const parseFile = async (url: string) => {
@@ -38,6 +25,7 @@ const TestDetailWindow: React.FC<{ result: any }> = ({ result }) => {
         const worksheet = workbook.Sheets[firstSheetName]
         const jsonData = XLSX.utils.sheet_to_json(worksheet)
         setParsedData(jsonData)
+        setShowDetails(false)
       }
       reader.readAsArrayBuffer(blob)
     } catch (error) {
@@ -45,56 +33,71 @@ const TestDetailWindow: React.FC<{ result: any }> = ({ result }) => {
     }
   }
 
+  // Automatically parse file when resultData.url is available
+  useEffect(() => {
+    if (result.resultData?.url) {
+      parseFile(result.resultData.url)
+    }
+  }, [result.resultData?.url])
+
   return (
-    <div className="flex items-center justify-center max-h-[80vh] my-auto  bg-white">
-      <div className="relative bg-blue-100/70 p-8 rounded shadow-md w-full max-w-[75vw] max-h-[80vh] mb-14 overflow-hidden">
+    <div className="flex items-center justify-center ">
+      <div className="relative bg-blue-100/20 p-8 rounded shadow-md w-full max-w-[75vw] max-h-[86vh] my-8 overflow-auto">
         <Box className="absolute top-4 left-4">
           <IconButton onClick={() => navigate(-1)} color="primary" aria-label="go back">
             <ArrowBackIcon />
           </IconButton>
         </Box>
 
-        <Box className="pl-6">
-          <Typography variant="h6" gutterBottom className="pt-4 !font-bold">
-            Detail výsledku športového testu
-          </Typography>
-          <Typography variant="body1">
-            Dátum: {new Date(result?.date).toLocaleDateString()}
-          </Typography>
-          <Typography variant="body1">Typ testu: {result.testType?.name || 'N/A'}</Typography>
-          <Typography variant="body1">Poznámky: {result.notes || 'Žiadne poznámky'}</Typography>
-          {result.resultData?.url && (
-            <>
+        <Typography variant="h6" gutterBottom className="pt-4 pl-6 !font-bold">
+          Detail výsledku športového testu
+        </Typography>
+
+        {showDetails && (
+          <Box className="pl-6">
+            <Typography variant="body1">
+              Dátum: {new Date(result?.date).toLocaleDateString()}
+            </Typography>
+            <Typography variant="body1">Typ testu: {result.testType?.name || 'N/A'}</Typography>
+            <Typography variant="body1">Poznámky: {result.notes || 'Žiadne poznámky'}</Typography>
+            {result.resultData?.url && (
               <Typography>
                 Súbor:{' '}
                 <Link href={result.resultData.url} target="_blank">
                   {result.resultData.title || 'Stiahnuť'}
                 </Link>
               </Typography>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={() => parseFile(result.resultData.url)}
-                sx={{ mt: 2 }}
-              >
-                Zobraziť dáta zo súboru
-              </Button>
-            </>
-          )}
-        </Box>
+            )}
+          </Box>
+        )}
 
         {parsedData.length > 0 && (
-          <Box className="pl-6">
-            <Box>
-              {['Name', 'Age', 'Height'].map(key => {
+          <Box className="p-6 flex flex-row gap-6">
+            <Box className="flex flex-col gap-2">
+              {['Name', 'Group', 'Age', 'Test Date'].map(key => {
                 const paramKey = Object.keys(params).find(k => k.includes(key))
                 const label = paramKey ? params[paramKey] : key
                 const value = paramKey ? parsedData[0][paramKey] : 'N/A'
 
                 return (
-                  <Card key={key} className="w-52 my-2 p-4">
+                  <Card key={key} className="w-52 p-4">
                     <Typography variant="body1" className="font-bold">
-                      {label}: {value} {key === 'Age' ? 'rokov' : key === 'Height' ? 'cm' : ''}
+                      {label}: {value} {key === 'Age' ? 'rokov' : ''}
+                    </Typography>
+                  </Card>
+                )
+              })}
+            </Box>
+            <Box className="flex flex-col gap-2">
+              {['Height', 'Weight'].map(key => {
+                const paramKey = Object.keys(params).find(k => k.includes(key))
+                const label = paramKey ? params[paramKey] : key
+                const value = paramKey ? parsedData[0][paramKey] : 'N/A'
+
+                return (
+                  <Card key={key} className="w-52 p-4">
+                    <Typography variant="body1" className="font-bold">
+                      {label}: {value} {key === 'Height' ? 'cm' : key === 'Weight' ? 'kg' : ''}
                     </Typography>
                   </Card>
                 )
