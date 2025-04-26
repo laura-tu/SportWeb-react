50ee0621fb53b5afd8bd16cc9589ca79eba1e0ee
laura <brejcakova.laura@gmail.com>
2025-04-26 22:00:19 +0200

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import HomeView from './components/homeview/index'
 import DashboardLayoutAccount from './components/dashboard'
 import 'tailwindcss'
