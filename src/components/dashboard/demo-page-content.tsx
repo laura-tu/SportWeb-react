98fad3e94655ceae06b5b3878547eec822adefa2
laura <brejcakova.laura@gmail.com>
2025-06-06 14:47:34 +0200

    dashboard design theme

--- src/components/dashboard/demo-page-content.tsx
+++ src/components/dashboard/demo-page-content.tsx
@@ -5,6 +5,9 @@ import { useFetchUserById } from '@/api/hooks/useUserQuery'
 import LoadingSpinner from '../loading/loading-spinner'
 import { useFetchAthlete } from '@/api/hooks/useAthleteQuery'
 import { Badge } from '@/components/ui/badge'
+import Heading from '../heading'
+import { useTheme } from '@mui/material/styles'
+import { cn } from '@/utils/cn'
 
 interface DemoPageProps {
   userId: string
@@ -34,6 +37,9 @@ const DemoPageContent: React.FC<DemoPageProps> = ({ userId }) => {
     return () => clearInterval(timer)
   }, [])
 
+  const theme = useTheme()
+  const isDarkMode = theme.palette.mode === 'dark'
+
   if (isLoading) return <LoadingSpinner />
 
   if (error) {
@@ -46,38 +52,52 @@ const DemoPageContent: React.FC<DemoPageProps> = ({ userId }) => {
 
   return (
     <div className="m-5">
-      <Box direction="row" className="p-2">
-        <Typography
-          sx={{ color: theme => (theme.palette.mode === 'dark' ? 'white' : 'black') }}
-          variant="h4"
-        >
-          Vitaj, {userData?.name}
-        </Typography>
-      </Box>
-
       <Box direction="row" className="gap-2 m-2">
-        <Box direction="col" className="border-2 h-[25rem] flex-2">
-          1
+        <Box
+          direction="col"
+          className={cn(
+            'border-2 border-gray-400 rounded-3xl h-[25rem] flex-2 items-start p-8 gap-6',
+            isDarkMode ? 'bg-gray-900 text-white' : 'bg-gray-100 text-black',
+          )}
+        >
+          <Typography
+            sx={{ color: theme => (theme.palette.mode === 'dark' ? 'white' : 'black') }}
+            variant="h4"
+          >
+            Vitaj späť, {userData?.name}!
+          </Typography>
+          <Heading
+            level={6}
+            text="Teší nás, že si opäť tu. Sleduj svoj pokrok, porovnávaj výsledky testov a pracuj na
+            svojom výkone krok za krokom."
+            className="text-inherit pt-4 w-full"
+          />
         </Box>
 
         <Box direction="col" className="flex-1 h-[25rem] gap-2">
           <Box
             direction="col"
-            className="border-2 border-gray-400 rounded-3xl bg-blue-100 flex-1 justify-center items-center p-2"
+            className={cn(
+              'border-2 border-gray-400 rounded-3xl flex-1 justify-center items-center p-2',
+              isDarkMode ? 'bg-blue-900 text-white' : 'bg-blue-100 text-black',
+            )}
           >
-            <Typography variant="subtitle1" className="text-center text-gray-500">
+            <Typography variant="subtitle1" className="text-center text-inherit">
               Aktuálny dátum a čas:
             </Typography>
-            <Typography variant="h6" className="text-center">
+            <Typography variant="h6" className="text-center text-inherit">
               {currentTime}
             </Typography>
           </Box>
 
           <Box
             direction="col"
-            className="border-2 border-gray-400 rounded-3xl bg-blue-100 flex-1 justify-start items-start p-8"
+            className={cn(
+              'border-2 border-gray-400 rounded-3xl flex-1 justify-start items-start p-8',
+              isDarkMode ? 'bg-blue-900 text-white' : 'bg-blue-100 text-black',
+            )}
           >
-            <Typography variant="subtitle1" className="text-left text-gray-500 pb-4">
+            <Typography variant="subtitle1" className="text-left text-inherit pb-4">
               Toto je zoznam športov, ktorým sa venuješ:
             </Typography>
 
